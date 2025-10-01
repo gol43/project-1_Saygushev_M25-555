2ea3f0d562d88c3c6655fd0c93b82b616b63a407
Сайгушев Дамир <saigh06@gmail.com>
2025-10-01 14:44:17 +0200

    Добавление утилит: Случайные события и генераторы

--- labyrinth_game/utils.py
+++ labyrinth_game/utils.py
@@ -1,4 +1,5 @@
 from labyrinth_game.constants import ROOMS
+import math
 
 
 def describe_current_room(game_state):
@@ -81,3 +82,51 @@ def show_help():
     print("  solve           - попытаться решить загадку в комнате")
     print("  quit            - выйти из игры")
     print("  help            - показать это сообщение") 
+
+
+def pseudo_random(seed, modulo):
+    x = math.sin(seed * 12.9898) * 43758.5453
+    frac = x - math.floor(x)
+    return int(frac * modulo)
+
+
+def trigger_trap(game_state):
+    print("Ловушка активирована! Пол стал дрожать...")
+    inventory = game_state['player_inventory']
+    seed = game_state['steps_taken']
+    if inventory:
+        idx_for_delete_item = pseudo_random(seed, len(inventory))
+        name_of_deleted_item = inventory.pop(idx_for_delete_item)
+        print(f"Вы потеряли предмет: {name_of_deleted_item}!")
+    else:
+        damage_roll = pseudo_random(seed, 10)
+        if damage_roll < 3:
+            print("Вы попали в ловушку и не выжили. Игра окончена.")
+            game_state['game_over'] = True
+        else:
+            print("Ловушка сработала, но вы живы.")
+
+
+def random_event(game_state: dict):
+    room = game_state['current_room']
+    steps =  game_state['steps_taken']
+    current_room = ROOMS[room]
+    if pseudo_random(steps, 10) != 0:
+        return
+
+    event_type = pseudo_random(steps + 1, 3)
+
+    if event_type == 0: # Находка монетки
+        print("Вы находите на полу монетку.")
+        current_room['items'].append('coin')
+
+    elif event_type == 1: # Испуг
+        print("Вы слышите шорох. Вам должно быть страшно.")
+        if 'sword' in game_state['player_inventory']:
+            print("Но ваш меч отпугнул существо.")
+
+    elif event_type == 2:  # Ловушка
+        if (room == 'trap_room'
+            and 'torch' not in game_state['player_inventory']):
+            print("Опасность! Ловушка может сработать.")
+            trigger_trap(game_state)
